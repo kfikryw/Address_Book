24ec6b809b6dd4069fbab33cc9f41b25f280e42c
kfikryw <110513666+kfikryw@users.noreply.github.com>
2024-02-18 12:26:07 +0800

    Delete CSS and HTML/Address_Book directory

--- CSS and HTML/Address_Book/UI Address Book/main.js
+++
@@ -1,7 +0,0 @@
-import { addFriend,deleteFriend,updateFriend,displayFriendList } from "./function.js";
-
-//event listener
-document.getElementById("addButton").addEventListener("click",addFriend);
-document.getElementById("deleteButton").addEventListener("click",deleteFriend);
-document.getElementById("updateButton").addEventListener("click",updateFriend);
-displayFriendList();
